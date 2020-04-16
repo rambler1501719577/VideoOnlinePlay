14d3f6cb9c3548c348f8421ccd8b8913e595e80b
rambler1501719577 <1501719577@qq.com>
2020-04-17 00:25:38 +0800

    首页初步效果

--- src/main/java/com/rambler/controller/BaseController.java
+++ src/main/java/com/rambler/controller/BaseController.java
@@ -1,13 +1,10 @@
 package com.rambler.controller;
 
-import com.rambler.config.Response;
 import com.rambler.config.Variable;
 import com.rambler.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -15,23 +12,14 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class BaseController {
 
-    @Autowired
-    private MenuService menuService;
-
     @RequestMapping("/")
     public String index(HttpServletRequest request) {
         // 检测用户是否登陆, 如果未登录跳转登陆页面, 否则跳到首页
         HttpSession session = request.getSession();
         if (session.getAttribute(Variable.CURRENT_USER) != null) {
-            return "index.html";
+            return "index/index.ftl";
         }
-        return "login.html";
-//        return "index.html";
+        return "common/login.html";
     }
 
-    @RequestMapping(value = "menu", method = RequestMethod.GET)
-    @ResponseBody
-    public Response getSystemMenu(HttpServletRequest request) {
-        return menuService.searchMenuByRole(request);
-    }
 }
--- src/main/java/com/rambler/controller/PageController.java
+++ src/main/java/com/rambler/controller/PageController.java
@@ -1,12 +1,17 @@
 package com.rambler.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author rambler
  * @since 2020-02-02 21:17
  */
 @Controller
+@RequestMapping("page")
 public class PageController {
-
+    @RequestMapping("detail")
+    public String detailPage(){
+        return "index/detail.ftl";
+    }
 }
